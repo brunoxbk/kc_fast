2bcec469f5c19df103dbf3d52ec1ddcb40fad159
Wesley <brunoxbk@gmail.com>
2024-04-24 14:14:43 -0300

    get current user

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2AuthorizationCodeBearer
-
 from keycloak import KeycloakOpenID
-
 from typing import Annotated
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+# {'sub': '10373295-3342-4ea0-bfa7-a39a3b750f3c',
+#  'email_verified': True, 'name': 'teste teste',
+#  'preferred_username': 'teste',
+#  'given_name': 'teste', 'family_name': 'teste','email': 'teste@gmail.com'}
+
+
+# {'exp': 1713963449, 'iat': 1713963149, 'jti': '687e9d22-c510-4bf8-a709-7c77d96a8d18', 
+#  'iss': 'http://localhost:8080/realms/CarShip', 'aud': 'account', 'sub': '10373295-3342-4ea0-bfa7-a39a3b750f3c',
+#  'typ': 'Bearer', 'azp': 'carship-client', 'session_state': 'df093aa2-fc52-4fda-9f61-e823d555239a',
+#  'acr': '1', 'realm_access': {'roles': ['offline_access', 'uma_authorization', 'default-roles-carship']}, 
+#  'resource_access': {'account': {'roles': ['manage-account', 'manage-account-links', 'view-profile']}},
+#    'scope': 'openid profile email', 'sid': 'df093aa2-fc52-4fda-9f61-e823d555239a', 
+#    'email_verified': True, 'name': 'teste teste', 'preferred_username': 'teste', 
+#    'given_name': 'teste', 'family_name': 'teste', 'email': 'teste@gmail.com'}
+
+class User(BaseModel):
+    uuid: str
+    username: str
+    email: str | None = None
+    email_verified: bool
+    full_name: str | None = None
+    disabled: bool | None = None
+
 
 oauth_2_scheme = OAuth2AuthorizationCodeBearer(
     tokenUrl="http://localhost:8080/realm/protocol/openid-connect/token",
@@ -13,28 +38,60 @@ oauth_2_scheme = OAuth2AuthorizationCodeBearer(
     refreshUrl="http://localhost:8080/realm/protocol/openid-connect/token",
 )
 
-keycloak_openid = KeycloakOpenID(server_url="http://localhost:8080/",
-                                 client_id="carship-client",
-                                 realm_name="CarShip",
-                                 client_secret_key="B2Lr0jdRV1Vk3eg9BZjd57oMwXZaMmqt")
+keycloak_openid = KeycloakOpenID(
+    server_url="http://localhost:8080/",
+    client_id="carship-client",
+    realm_name="CarShip",
+    client_secret_key="B2Lr0jdRV1Vk3eg9BZjd57oMwXZaMmqt")
+
+
+def decode_token(token):
+    KEYCLOAK_PUBLIC_KEY = f"-----BEGIN PUBLIC KEY-----\n{keycloak_openid.public_key()}\n-----END PUBLIC KEY-----"
+
+    options = {
+        "verify_signature": True,
+        "verify_aud": False,
+        "verify_exp": True
+    }
+
+    token_info = keycloak_openid.decode_token(
+        token,
+        key=KEYCLOAK_PUBLIC_KEY,
+        options=options
+    )
+    return User(
+        uuid=token_info['sid'],
+        username=token_info['name'], 
+        email=token_info['email'],
+        email_verified=token_info['email_verified'],
+        full_name=token_info['name']
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth_2_scheme)]):
+    user = decode_token(token)
+    return user
 
 
 async def valid_access_token(
     access_token: Annotated[str, Depends(oauth_2_scheme)]
 ):
     try:
-        print(access_token)
-        print('aqui')
-        userinfo = keycloak_openid.userinfo(access_token)
-        print(userinfo)
+        
+        user_info = keycloak_openid.userinfo(access_token)
 
         KEYCLOAK_PUBLIC_KEY = f"-----BEGIN PUBLIC KEY-----\n{keycloak_openid.public_key()}\n-----END PUBLIC KEY-----"
         
-        options = {"verify_signature": True, "verify_aud": False, "verify_exp": True}
+        options = {
+            "verify_signature": True,
+            "verify_aud": False,
+            "verify_exp": True
+        }
         
-        token_info = keycloak_openid.decode_token(access_token, key=KEYCLOAK_PUBLIC_KEY, options=options)
-
-        print(token_info)
+        token_info = keycloak_openid.decode_token(
+            access_token,
+            key=KEYCLOAK_PUBLIC_KEY,
+            options=options
+        )
 
         return {"token_info": token_info, "user_info": user_info}
 
@@ -44,9 +101,9 @@ async def valid_access_token(
 
 def has_role(role_name: str):
     async def check_role(
-        token_data: Annotated[dict, Depends(valid_access_token)]
+        token_info: Annotated[dict, Depends(valid_access_token)]
     ):
-        roles = token_data["token_info"]["resource_access"]["account"]["roles"]
+        roles = token_info["resource_access"]["account"]["roles"]
         print(roles)
         if role_name not in roles:
             raise HTTPException(status_code=403, detail="Unauthorized access")
@@ -55,9 +112,8 @@ def has_role(role_name: str):
 
 
 @app.get("/user-info")
-def user_info():
-    userinfo = keycloak_openid.userinfo(token['access_token'])
-    return {"userinfo": userinfo}
+def user_info(current_user: Annotated[User, Depends(get_current_user)]):
+    return {"name": current_user.full_name, "email": current_user.email}
 
 
 @app.get("/public")
